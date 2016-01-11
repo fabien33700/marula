466f48497fcd80dcaa8fa4f6c295ce7ba7cc7957
fabien33700 <fabien.lehouedec@gmail.com>
2016-01-11 16:29:55 +0100

    phpDoc and header for NumericState class

--- utils/NumericState.php
+++ utils/NumericState.php
@@ -1,16 +1,27 @@
 <?php
 
+    /**
+	 * Marula Library, use easily treenodes in PHP !
+	 * 	 coded with and for PHP 5.6+
+     * Treenodes algorithm implementation for PHP 
+     *   (first of all, for personnal learning and skill improving purposes)
+     * @author Fabien Le Houëdec (git: fabien33700) <fabien.lehouedec@gmail.com>
+     */
     namespace Marula\Utils;
 
     abstract class NumericState
     {
         const PCRE_CHECK_INT   = "/^([+-]?)([0-9]+)$/mi";
-        
+
+        /**
+         * Check if a string represents a valid integer number.
+         * @param $numStr the string to check
+         * @return boolean
+         */         
         public static function strIsInt($numStr)
         {
             return preg_match(self::PCRE_CHECK_INT, $numStr);
         }
-    
     }
     
 ?>

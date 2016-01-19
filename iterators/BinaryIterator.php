ac4f49719868d5bfe1cd64d39e462e62c5c6c178
fabien33700 <fabien.lehouedec@gmail.com>
2016-01-19 22:45:28 +0100

    Removed \t tabulations --> replaced by whitespace

--- iterators/BinaryIterator.php
+++ iterators/BinaryIterator.php
@@ -1,13 +1,13 @@
 <?php
 
     /**
-	 * Marula Library, use easily treenodes in PHP !
-	 * 	 coded with and for PHP 5.6+
+     * Marula Library, use easily treenodes in PHP !
+     *   coded with and for PHP 5.6+
      * Treenodes algorithm implementation for PHP 
      *   (first of all, for personnal learning and skill improving purposes)
      * @author Fabien LH (git: fabien33700) <fabien DOT lehouedec AT gmail DOT com>
      */
-	namespace Marula\Iterators;
+    namespace Marula\Iterators;
 
     use Marula\Iterators\BinaryNodeIterator,
         Marula\Core\AbstractNode,
